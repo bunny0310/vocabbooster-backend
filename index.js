045e81383db7cc5f61c27e0ca533520b3293aa57
Ishaan Khurana <khuranaishaan5@gmail.com>
2020-06-17 21:13:14 -0400

    06/17/2020 Authorization implemented using passport library

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const body_parser = require("body-parser");
 const parseJson = require('parse-json');
 const mysql = require("mysql");
+const sha256 = require("sha256");
+const passport =require("passport");
+const localStrategy =require("passport-local").Strategy;
 const path = require("path");
 const Word = require("./Word/word");
 const Wordlist = require("./Word/word_list");
@@ -16,11 +19,33 @@ const random_words = new Wordlist();
 app.use(cors());
 app.use(body_parser.json())
 app.use(express.static(path.join(__dirname,'/public')));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new localStrategy((username,password,done)=>{
+    asyncQueryMethod("SELECT * FROM users WHERE username = '"+username+"' AND password = '"+sha256(password)+"'")
+    .then(rows=>{
+        console.log(password);
+        if(rows.length!==1)return done("401 unauthorized access", false);
+        const row = rows[0];
+        console.log(row);
+        return done(null,username);
+    });
+
+}))
+
+const isLoggedIn = (req, res, next) => {
+    if(req.isAuthenticated()){
+        return next()
+    }
+    return res.status(401).json({"statusCode" : "401", "message" : "not authenticated"})
+}
 
 //wordlist.list.push(new Word({name: 'grotesque', type: ['noun'], meaning: 'comically or repulsively distorted or ugly', synonyms: ['disfigured']}));
 
-app.get('/api/words', (req,res)=>{
-        asyncQueryMethod("SELECT word_json FROM words").then(rows=>{
+app.post('/api/words', isLoggedIn, (req,res)=>{
+        if(req.body.username === '')return res.status(400).json({"error": "bad request"});
+        asyncQueryMethod("SELECT word_json FROM words WHERE username = '"+req.body.username+"'").then(rows=>{
             for(let row of rows)
             {
                 const word = parseJson(row.word_json);
@@ -46,15 +71,16 @@ app.post('/api/add-word', (req,res)=>{
     //console.log(word_main);
     wordlist.addWord(word_main);
     const word_json = JSON.stringify(word_main);
-    asyncQueryMethod("INSERT INTO words (word_json) VALUES ('"+word_json+"')").catch((error)=>{
+    asyncQueryMethod("INSERT INTO words (word_json, username) VALUES ('"+word_json+"', '"+req.body.username+"')").catch((error)=>{
       console.log(error);
     });
     return res.status(200).json({"word added!":"awesome"});
 });
 
-app.get('/api/random-words', (req,res)=>{
+app.post('/api/random-words', (req,res)=>{
     random_words.list = [];
-    asyncQueryMethod("SELECT word_json FROM words ORDER BY RAND() LIMIT 5").then(rows=>{
+    if(req.body.username === '')return res.status(400).json({"error": "bad request"});
+    asyncQueryMethod("SELECT word_json FROM words WHERE username = '"+req.body.username+"' ORDER BY RAND() LIMIT 5").then(rows=>{
         for(let row of rows)
         {
             const word = parseJson(row.word_json);
@@ -80,6 +106,45 @@ const saveAsync = async ()=>{
     return r;
 };
 
+
+//authentication
+
+const auth = () => {
+    return (req, res, next) => {
+        passport.authenticate('local', (error, user, info) => {
+            if(error) res.status(401).json({"statusCode" : 401 ,"message" : error});
+            req.login(user, function(error) {
+                if (error) return next(error);
+                next();
+            });
+        })(req, res, next);
+    }
+}
+
+passport.serializeUser(function(user, done) {
+    if(user) done(null, user);
+});
+  
+passport.deserializeUser(function(id, done) {
+    done(null, id);
+});
+
+app.post('/api/login', auth(), (req,res)=>{
+   return res.status(200).json({"username":req.body.username});
+});
+
+app.post('/api/register', (req,res)=>{
+    if(req.body.username==='' || req.body.password==='')
+        return res.status(400).json({"error": "null values found"});
+    const query = "INSERT INTO users (username,password) VALUES ('"+req.body.username+"', '"+sha256(req.body.password)+"')";
+    asyncQueryMethod(query).catch((error)=>console.log(error));
+    return res.status(200).json({"message": "user added!"});
+ });
+
+
+
+
+//listen 
 app.listen(PORT, ()=>{
     console.log("Running on port ", PORT);
 });
