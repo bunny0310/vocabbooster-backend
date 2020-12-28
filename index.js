d42880c64f1f5a572b6be26e4049029180865ae7
Ishaan Khurana <khuranaishaan5@gmail.com>
2020-12-28 03:04:22 -0500

    12/28/2020 authentication system revamped, using jwt instead of sessions

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ const Graph = require("./Graph");
 const {getAdjLists} = require("./util");
 const session = require("express-session");
 const request = require("request");
+const passportJWT = require('passport-jwt');
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+const JWTStrategy = passportJWT.Strategy;
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const wordlist = new Wordlist(true);
@@ -49,36 +53,83 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.set('trust proxy', 1);
 
+//set jwt options
+var options = {};
+options.jwtFromRequest = ExtractJWT.fromAuthHeaderAsBearerToken();
+options.secretOrKey = 'secret123'; 
+
 passport.use(new localStrategy((username,password,done)=>{
     asyncQueryMethod("SELECT * FROM users WHERE username = '"+username+"' AND password = '"+sha256(password)+"'")
     .then(rows=>{
-        console.log(password);
         if(rows.length!==1)return done(null, false, {
             "message": "incorrect credentials"
         });
         const row = rows[0];
-        console.log(row);
+        // console.log(row);
         return done(null,username);
     });
 
 }))
 
-const isLoggedIn = (req, res, next) => {
-    console.log(req.isAuthenticated());
-    console.log(req.session);
-    if(req.isAuthenticated()){
-        return next()
-    }
-    return res.status(401).json({"statusCode" : "401", "message" : "not authenticated"})
-}
+//jwt strategy
+passport.use(new JWTStrategy(options, function(jwtPayload, done) {
+        console.log(jwtPayload);
+        return done(null, jwtPayload);
+}))
+
 
 let graph = new Friends();
+
+//authentication
+app.post('/api/login', function(req, res, next) {
+    passport.authenticate('local', {session: false}, function(err, user, info) {
+        
+        if (err) { return next(err); }
+
+        if (!user) {
+            return res.status(500).json(info.message)
+        }
+        const payload = {
+            username: user
+        }
+        const options = {
+            subject: user,
+            expiresIn: 3600
+        }
+        const token = jwt.sign(payload, 'secret123', options);
+        
+        res.json({token});
+
+    })(req, res, next);
+})
+
+app.post('/api/register', (req,res)=>{
+    if(req.body.username==='' || req.body.password==='')
+        return res.status(400).json({"error": "null values found"});
+    const query = "INSERT INTO users (username,password) VALUES ('"+req.body.username+"', '"+sha256(req.body.password)+"')";
+    asyncQueryMethod(query).catch((error)=>console.log(error));
+    return res.status(200).json({"message": "user added!"});
+ });
+
+ //middleware to verify authentication
+const isLoggedIn = passport.authenticate('jwt', { session: false });
+
+ app.get('/logout', isLoggedIn,  function (req, res) {
+    req.logout();
+    req.session.destroy();
+    return res.status(200).json({"message": "logged out"});
+});
+
+app.get('/isLoggedIn', isLoggedIn, (req, res) => {
+    console.log('is logged in working!');
+    return res.status(200).json({"message": "logged in!"});
+});
 
 //get words
 app.post('/api/words', isLoggedIn, (req,res,next)=>{
         if(req.body.username === '')return res.status(400).json({"error": "bad request"});
         wordlist.list = [];
-        asyncQueryMethod("SELECT word_json FROM words").then(rows=>{
+        asyncQueryMethod("SELECT word_json FROM words WHERE username = '" + req.body.username + "'").then(rows=>{
             for(let row of rows)
             {
                 const word = parseJson(row.word_json);
@@ -172,7 +223,7 @@ app.post('/api/search-words', isLoggedIn, (req,res,next)=>{
 });
 
 //add word
-app.post('/api/add-word', (req,res)=>{
+app.post('/api/add-word', isLoggedIn, (req,res)=>{
     let word = req.body.word;
     word=parseJson(word);
     console.log(typeof word);
@@ -231,52 +282,6 @@ app.post('/api/get-friends', (req,res)=>{
     //     return res.status(200).json({graph});
     // })
 });
-
-
-//authentication
-
-const auth = () => {
-    return (req, res, next) => {
-        passport.authenticate('local', (error, user, info) => {
-            if(error) res.status(401).json({"statusCode" : 401 ,"message" : error});
-            req.login(user, function(error) {
-                if (error) return next(error);
-                next();
-            });
-        })(req, res, next);
-    }
-}
-
-passport.serializeUser(function(user, done) {
-    if(user) done(null, user);
-});
-  
-passport.deserializeUser(function(id, done) {
-    done(null, id);
-});
-
-app.post('/api/login', auth(), (req,res,next)=>{
-    req.session.save();
-   return res.status(200).json({"username":req.body.username});
-});
-
-app.post('/api/register', (req,res)=>{
-    if(req.body.username==='' || req.body.password==='')
-        return res.status(400).json({"error": "null values found"});
-    const query = "INSERT INTO users (username,password) VALUES ('"+req.body.username+"', '"+sha256(req.body.password)+"')";
-    asyncQueryMethod(query).catch((error)=>console.log(error));
-    return res.status(200).json({"message": "user added!"});
- });
- app.get('/logout', isLoggedIn,  function (req, res) {
-    req.logout();
-    req.session.destroy();
-    return res.status(200).json({"message": "logged out"});
-});
-
-app.get('/isLoggedIn', (req, res) => {
-    return res.status(200).json({"status": req.isAuthenticated()});
-});
-
 
 
 
